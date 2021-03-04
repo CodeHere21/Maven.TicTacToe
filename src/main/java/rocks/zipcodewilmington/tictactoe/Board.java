654d7700850dc86c2ac39a3a60cb8e6a067cfe4d
lena <alena.litouka@gmail.com>
2021-03-03 23:58:03 -0500

    Last test is not working

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,42 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    Character[][] gameBoard;
     public Board(Character[][] matrix) {
+     this.gameBoard=matrix;
     }
 
     public Boolean isInFavorOfX() {
-        return null;
-    }
+        if((gameBoard[0][1]==gameBoard[1][1])&&(gameBoard[0][1]==gameBoard[2][1])){
+            return true;}
+        else {return false;}
+  }
 
     public Boolean isInFavorOfO() {
-        return null;
+        if((gameBoard[0][1]==gameBoard[1][1])&&(gameBoard[0][1]==gameBoard[2][1])){
+        return true;}
+        else {return false;}
     }
 
     public Boolean isTie() {
-        return null;
+        Boolean flag=false;
+        if((gameBoard[0][0]==gameBoard[0][2]) ==( gameBoard[0][2] == gameBoard[2][0])){
+         flag=true;}
+        else {flag=false;}
+        return flag;
     }
 
     public String getWinner() {
-        return null;
+        for (int x=0; x< gameBoard.length; x++){
+            if((gameBoard[0][1]==gameBoard[1][1])&&(gameBoard[0][1]==gameBoard[2][1])){ return "Winner O";}
+        else if((gameBoard[0][1]==gameBoard[1][1])&&(gameBoard[0][1]==gameBoard[2][1])) {return "Winner X";}
+        else {
+                return "its tie";
+            }
+        }
+        return "null";
+    }
     }
 
-}
+
+
